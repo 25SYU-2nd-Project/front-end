75416453c3bba75393d2791dd84b2d5d1dcf9487
ryul00 <gju07059@gmail.com>
2025-06-19 16:59:54 +0900

    메인 화면 기본->녹음 중 전환 UI 구현 완료

--- src/app/Component/QuickRecord.tsx
+++ src/app/Component/QuickRecord.tsx
@@ -1,7 +1,68 @@
-export default function QuickRecord(){
-    return(
-        <div className="Quick-Record-Container">
-            <p>빠른 녹음 테스트</p>
+'use client'
+import '../Styles/QuickRecord.css'
+import Image from 'next/image'
+// import Gemeni from '../../../public/images/Gemini.png'
+// import Pause from '../../../public/images/QuickRecordPause.png'
+// import Play from '../../../public/images/QuickRecordPlay.png'
+
+export default function QuickRecord() {
+  return (
+    <div className='Recording-Quick-Record-Box'>
+      <div className='Recording-Record-Header'>
+        <Image className='Recording-Record-Header-Img' src="/images/blackmic.png" alt="MicLogo" width={24} height={24} />
+        <p className='Recording-Record-Header-Text'>빠른 녹음 및 요약</p>
+      </div>
+
+      <div className='Recording-Record-Voice-Box'>
+        <div className='Recording-Voice-Header'>
+          <Image className='Recording-Voice-Header-Img' src="/images/whitemic.png" alt="MicLogo2" width={24} height={24} />
+          <p className='Recording-Voice-Header-Text'>실시간 음성 기록 및 요약</p>
         </div>
-    )
+
+        <div className='Recording-Voice-Content'>
+          <div className='Recording-Voice-Content-Timer'>00:00:00</div>
+          <div className='Recording-Voice-Content-ButtonBox'>
+            <Image
+              className='Recording-Play-Button'
+              src="/images/QuickRecordPlay.png"
+              alt="PlayButton"
+              width={30}
+              height={30}
+            />
+            <Image
+              className='Recording-Gemini-Button'
+              src="/images/Gemini.png"
+              alt="GemButton"
+              width={27}
+              height={27}
+            />
+            <Image
+              className='Recording-Pause-Button'
+              src="/images/QuickRecordPause.png"
+              alt="PauseButton"
+              width={30}
+              height={30}
+            />
+          </div>
+          <div className='Recording-Gemini-Summary'>
+              회의 요약
+          </div>
+          <div className='Recording-Gemini-Save'>
+              <p className='Save-Label'>요약본 클립보드에 저장</p>
+          </div>
+        </div>
+      </div>
+
+      <div className='Record-Voice-Summary'>
+        <div className='Record-Voice-Summary-Header'>
+          <Image className='Record-Voice-Summary-Header-Img' src="/images/SummaryIcon.png" alt="SummaryLogo" width={16.31} height={20} />
+          <p className='Record-Voice-Summary-Header-Text'>텍스트 회의록 요약</p>
+        </div>
+        <div className='Record-Voice-Summary-Content'>
+          <textarea className='Voice-Summary' placeholder='회의록 텍스트를 입력해주세요.'></textarea>
+        </div>
+      </div>
+    </div>
+
+  )
 }
--- src/app/Main/page.tsx
+++ src/app/Main/page.tsx
@@ -10,7 +10,11 @@ import Image from 'next/image'
 
 export default function MainPage() {
   const router = useRouter()
-  const [showQuickRecord, setShowQuickRecord] = useState(false)
+
+  // Idle(기본) - Recording(녹음 시작 및 요약 컴포넌트) 전환 
+  const [recordingPhase, setRecordingPhase] = useState<'idle' | 'recording'>('idle');
+
+
 
   return (
     <div className='Main-Container'>
@@ -31,35 +35,52 @@ export default function MainPage() {
           </button>
         </div>
       </div>
-      <div className='Quick-Record-Box'>
-        <div className='Quick-Record-Header'>
-          <Image className='Quick-Record-Header-Img' src="/images/blackmic.png" alt="MicLogo" width={24} height={24} />
-          <p className='Quick-Record-Header-Text'>빠른 녹음 및 요약</p>
-        </div>
-        <div className='Record-Voice-Box'>
-          <div className='Record-Voice-Header'>
-            <Image className='Record-Voice-Header-Img' src="/images/whitemic.png" alt="MicLogo2" width={24} height={24} />
-            <p className='Record-Voice-Header-Text'>실시간 음성 기록 및 요약</p>
-          </div>
-          <div className='Record-Voice-Content'>
-            <div className='Record-Voice-Content-Timer'>
-              00:00:00
-            </div>
-            <div className='Record-Voice-Content-StartButton'>
-              <Image
-                className='Record-Voice-Button'
-                src="/images/RecordButton.png"
-                alt="RecButton"
-                width={27}
-                height={27}
-                onClick={() => setShowQuickRecord(true)}
-              />
-            </div>
-              {showQuickRecord && <QuickRecord />}
+      {recordingPhase === 'idle' ? (
+        <div className='idle-Quick-Record-Box'>
+          <div className='idle-Record-Header'>
+            <Image className='idle-Record-Header-Img' src="/images/blackmic.png" alt="MicLogo" width={24} height={24} />
+            <p className='idle-Record-Header-Text'>빠른 녹음 및 요약</p>
           </div>
 
+          <div className='idle-Record-Voice-Box'>
+            <div className='idle-Voice-Header'>
+              <Image className='idle-Voice-Header-Img' src="/images/whitemic.png" alt="MicLogo2" width={24} height={24} />
+              <p className='idle-Voice-Header-Text'>실시간 음성 기록 및 요약</p>
+            </div>
+
+            <div className='idle-Voice-Content'>
+              <div className='idle-Voice-Content-Timer'>00:00:00</div>
+              <div className='idle-Voice-Content-StartButton'>
+                <Image
+                  className='idle-Voice-Button'
+                  src="/images/RecordButton.png"
+                  alt="RecButton"
+                  width={27}
+                  height={27}
+                  onClick={() => setRecordingPhase('recording')}
+                />
+              </div>
+            </div>
+          </div>
+          <div className='Record-Voice-Summary'>
+            <div className='Record-Voice-Summary-Header'>
+              <Image className='Record-Voice-Summary-Header-Img' src="/images/SummaryIcon.png" alt="SummaryLogo" width={16.31} height={20} />
+              <p className='Record-Voice-Summary-Header-Text'>텍스트 회의록 요약</p>
+            </div>
+            <div className='Record-Voice-Summary-Content'>
+              <textarea className='Voice-Summary' placeholder='회의록 텍스트를 입력해주세요.'></textarea>
+            </div>
+          </div>
         </div>
-      </div>
+      ) : (
+        <QuickRecord />
+      )}
+
+
+
+
+
+
       <Footer />
     </div>
   )
