b15befdb1c6961b1161e402e0a74ce08de659a1a
ryul00 <gju07059@gmail.com>
2025-07-16 12:35:08 +0900

    백엔드 https 연결 완료

--- src/app/api.ts
+++ src/app/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://54.180.26.148:8080',
+  baseURL: 'https://thinktrip.it.com',
   headers: {
     'Content-Type': 'application/json',
   },
